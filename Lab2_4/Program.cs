6516b1d452bd4d4e196db13091d23c1e8ffe63db
belynick <112333179+belynick@users.noreply.github.com>
2022-09-19 23:29:26 +0300

    Add files via upload

---
+++ Lab2_4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            User user = new User();
+
+            user.Login = "qwerty123";
+            user.Name = "Ivan";
+            user.Surname = "Ivanov";
+            user.Age = 18;
+
+            Console.WriteLine("User:");
+            Console.WriteLine($"Login: {user.Login}");
+            Console.WriteLine($"Name: {user.Name}");
+            Console.WriteLine($"Surname: {user.Surname}");
+            Console.WriteLine($"Age: {user.Age}");
+            Console.WriteLine($"Date of the form: {user.FillingDate}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab2_4/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_4
+{
+    internal class User
+    {
+        private string _login;
+        private string _name;
+        private string _surname;
+        private int _age;
+        private DateTime _fillingDate;
+
+         public User()
+        {
+            _fillingDate = DateTime.UtcNow;
+        }
+
+
+        public string Login { get { return _login; } set { _login = value; } }
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Surname { get { return _surname; } set { _surname = value; } }
+        public int Age { get { return _age; } set { _age = value; } }
+        public DateTime FillingDate { get { return _fillingDate; } }
+    }
+}
+
+
